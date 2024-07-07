042815906679b3a6c87cb83eb234b2fbe3c57ae7
Codiku <111140836+codiku-dev@users.noreply.github.com>
2024-07-07 19:57:55 +0200

    wip

--- src/features/slide-input/slide-input.tsx
+++ src/features/slide-input/slide-input.tsx
@@ -3,12 +3,35 @@ import { tokyoNight } from "@uiw/codemirror-theme-tokyo-night";
 import CodeMirror from "@uiw/react-codemirror";
 import "./slide-input.css";
 import { Slide } from "@/types/slide.types";
+import * as prettier from "prettier";
+import tsParser from "prettier/parser-typescript";
 
+import { useEffect } from "react";
 export function SlideInput(p: {
   slide: Slide;
   style?: React.CSSProperties;
   onCodeChange: (code: string) => void;
 }) {
+  useEffect(() => {
+    const handleKeyDown = async (event: KeyboardEvent) => {
+      if (event.ctrlKey && event.key === "s") {
+        event.preventDefault();
+        const formattedCode = await prettier.format(p.slide.code, {
+          semi: false,
+          parser: "typescript",
+          plugins: [tsParser],
+        });
+        console.log("format code ", formattedCode);
+        p.onCodeChange(formattedCode);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [p.slide.code, p.onCodeChange]);
   return (
     <div className="p-2">
       <CodeMirror
