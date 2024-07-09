e88a81bbd7ba1c224ec4fa9042daec443b5caf8b
rlebhar-clone <robin.lebhar@getclone.io>
2024-07-09 12:25:19 +0200

    wip

--- src/pages/Home/constant.ts
+++ src/pages/Home/constant.ts
@@ -22,7 +22,7 @@ export const INITIAL_SLIDES: Slide[] = [
     imageList: [
       {
         id: "730ae962-66d9-4172-ad43-bbf15b5c1a73",
-        filePath: "https://code-presentation-six.vercel.app/point_left.png",
+        filePath: "https://code-prez.vercel.app/point_left.png",
         x: 497.9921875,
         y: 81.31640625,
       },
@@ -34,7 +34,7 @@ export const INITIAL_SLIDES: Slide[] = [
     imageList: [
       {
         id: "6386cca1-ddfd-45b2-bc8b-ce2e9e424249",
-        filePath: "https://code-presentation-six.vercel.app/point_left.png",
+        filePath: "https://code-prez.vercel.app/point_left.png",
         x: 497.9921875,
         y: 81.31640625,
       },
